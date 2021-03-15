e03906b426b5715e8f2af9a6fab4c5f5d66bad43
smau8 <sma2244@columbia.edu>
2021-03-14 22:41:05 -0700

    add paired programming

--- CanOpenness/__init__.py
+++ CanOpenness/__init__.py
@@ -2,6 +2,7 @@
 """
 Calculating canopy openness from photos program
 """
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from CanopyOpenness.CanopyOpenness import CanOpen
+from CanopyOpenness.CanopyOpenness import ImageLoad
