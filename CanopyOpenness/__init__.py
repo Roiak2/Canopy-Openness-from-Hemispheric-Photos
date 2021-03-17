708b22dc5a4df44973557326c210f7994d4e8bd6
Deren <de2356@columbia.edu>
2021-03-17 15:08:50 -0400

    renamed folder

---
+++ CanopyOpenness/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env/python
+"""
+Calculating canopy openness from photos program
+"""
+__version__ = "0.0.2"
+
+
+import sys
+from loguru import logger
+from CanopyOpenness import CanOpen
+from CanopyOpenness import ImageLoad
+
+
+def set_loglevel(loglevel="INFO"):
+    """
+    Set the loglevel for loguru logger. Using 'enable' here as 
+    described in the loguru docs for logging inside of a library.
+    """
+    config = {}
+    config["handlers"] = [{
+        "sink": sys.stdout,  # sys.stderr
+        "format": "{time:hh:mm} | {level: <7} | <b><magenta>{function: <15}</magenta></b> | <level>{message}</level>",
+        "level": loglevel,
+        "colorize": True, # sys.stdout.isatty() 
+    }]
+    logger.configure(**config)
+    logger.enable("CanopyOpenness")
