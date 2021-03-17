b2b647d3a76bc17c741d618f4aa3da3a3ed085b6
Deren <de2356@columbia.edu>
2021-03-17 15:14:38 -0400

    logger docstring notes added

--- CanopyOpenness/__init__.py
+++ CanopyOpenness/__init__.py
@@ -15,6 +15,20 @@ def set_loglevel(loglevel="INFO"):
     """
     Set the loglevel for loguru logger. Using 'enable' here as 
     described in the loguru docs for logging inside of a library.
+    This sets the level at which logger calls will be displayed 
+    throughout the rest of the code.
+
+    Example:
+      # example notebook usage:
+      import CanopyOpenness
+      CanopyOpenness.set_loglevel("DEBUG")
+      ...
+
+      # example statements written inside your code:
+      from loguru import logger
+      ...
+      logger.debug("starting a new analysis")
+      ...
     """
     config = {}
     config["handlers"] = [{
