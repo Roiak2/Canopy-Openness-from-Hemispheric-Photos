ac3887d44f0a8d13ec028cfe5539679f15ae56dd
Roiak2 <ra3040@columbia.edu>
2021-03-29 19:37:11 -0400

    Updating setup file to reflect new module

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 #building command
 setup(
     name = "CanopyOpenness",
-    version = "0.0.2",
+    version = "0.0.3",
     author = "Roí A.K.",
     author_email = "ra3040@columbia.edu",
     license = "GPLv3",
